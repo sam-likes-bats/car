68c0c0c81ad2c235f07cabc4d49e35b41008f976
sam-likes-bats <140191174+sam-likes-bats@users.noreply.github.com>
2023-11-16 15:39:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ var fuels, powerCoins, coinImage, fuelImage;
 
 //BP
 function preload() {
-  backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("./assets/car1.png");
-  car2_img = loadImage("./assets/car2.png");
-  track = loadImage("./assets/track.jpg");
-  coinImage = loadImage("./assets/goldCoin.png");
-  fuelImage = loadImage("./assets/fuel.png");
+  backgroundImage = loadImage("../assets/background.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+  coinImage = loadImage("../assets/goldCoin.png");
+  fuelImage = loadImage("../assets/fuel.png");
 }
 
 //BP
