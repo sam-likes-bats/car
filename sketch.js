97e2bcfe579148512cee7b10a5b2a85884284052
sam-likes-bats <140191174+sam-likes-bats@users.noreply.github.com>
2023-11-16 15:26:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,33 +5,39 @@ var form, player, playerCount;
 var allPlayers, car1, car2;
 var cars = [];
 
+//BP
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
-
-  car1_img = loadImage ("assets/car1.png");
-  car2_img = loadImage("assets/car2.png");
-  track = loadImage ("assets/track.jpg");
-
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+  coinImage = loadImage("./assets/goldCoin.png");
+  fuelImage = loadImage("./assets/fuel.png");
 }
 
+//BP
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
-  bgImg = backgroundImage;
+ 
 }
 
+//BP
 function draw() {
-  background(bgImg);
-  // atualize estado do jogo
- 
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
 
   if (gameState === 1) {
     game.play();
   }
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
